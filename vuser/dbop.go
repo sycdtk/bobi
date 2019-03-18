07ff331a218c4c466b203c64ffbc15d89478faec
sre <lirui_bj@lakala.com>
2019-03-18 01:56:36 +0100

    增加用户组操作

--- vuser/dbop.go
+++ vuser/dbop.go
@@ -5,6 +5,7 @@ import (
 	"github.com/sycdtk/bobi/orm"
 )
 
+//用户
 func (user *User) Save() {
 	cols := []string{"id", "name", "description", "gender", "username",
 		"password", "email", "telephone", "mobile", "logining", "locked",
@@ -37,6 +38,36 @@ func (user *User) Delete() {
 	orm.DeleteByID([]interface{}{user})
 }
 
+//用户组
+func (group *Group) Save() {
+	cols := []string{"id", "name", "description"}
+	orm.Create([]interface{}{group}, cols)
+
+	if group.Users != nil && !group.Users.Empty() {
+		for _, user := range group.Users.Array() {
+			orm.Create([]interface{}{NewUserGroup(user, group.ID)}, []string{"id", "userid", "groupid"})
+		}
+	}
+}
+
+func (group *Group) Update() {
+	cols := []string{"id", "name", "description"}
+	orm.Update([]interface{}{group}, cols, []string{"id"})
+
+	if group.Users != nil && !group.Users.Empty() {
+		orm.Delete([]interface{}{NewUserGroup("", group.ID)}, []string{"groupid"})
+		for _, user := range group.Users.Array() {
+			orm.Create([]interface{}{NewUserGroup(user, group.ID)}, []string{"userid", "groupid"})
+		}
+	}
+}
+
+func (group *Group) Delete() {
+	orm.Delete([]interface{}{NewUserGroup("", group.ID)}, []string{"groupid"})
+	orm.DeleteByID([]interface{}{group})
+}
+
+//查询函数
 func QueryByUsername(username string) *User {
 	cols := []string{"id", "name", "description", "gender", "username",
 		"password", "email", "telephone", "mobile", "logining", "locked",
@@ -108,13 +139,25 @@ func init() {
 		logger.Info("create table:", tn)
 	}
 
-	orm.Register("vuser", func() interface{} { return &UserOrganization{} })
+	orm.Register("vuser", func() interface{} { return &Group{} })
 	if tn, ok := orm.TableObjExist(&UserOrganization{}); !ok {
 		orm.Execute(`CREATE TABLE ` +
 			tn +
 			`(
 				userid TEXT,
 				organizationid TEXT
+		    );`)
+		logger.Info("create table:", tn)
+	}
+
+	orm.Register("vuser", func() interface{} { return &User{} })
+	if tn, ok := orm.TableObjExist(&User{}); !ok {
+		orm.Execute(`CREATE TABLE ` +
+			tn +
+			`(
+				id TEXT,
+				name TEXT,
+				description TEXT
 		    );`)
 		logger.Info("create table:", tn)
 	}
--- vuser/user_test.go
+++ vuser/user_test.go
@@ -40,13 +40,32 @@ func BenchmarkUser(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 
-		user := QueryByUsername("wolffy")
+		user := NewUser("wolffy", "test user", Male, "wolffy", "123456", "78306909@qq.com",
+			"010-12345678", "13520040000", "", set.NewSet(), set.NewSet())
+		user.Save()
+		user = QueryByUsername("wolffy")
 
-		if user != nil {
-			//b.Log("user != nil")
-		} else {
-			b.Error("user == nil")
-		}
+		groups := set.NewSet()
+		groups.Add("111111")
+		groups.Add("22222")
+		user.Groups = groups
+
+		user.Telephone = "010-12345678"
+		user.Update()
+
+		user = QueryByID(user.ID)
+		user.Name = "niky"
+		user.Update()
+		user = QueryByID(user.ID)
+		user.Name = "niky"
+		user.Update()
+		user = QueryByID(user.ID)
+		user.Name = "niky"
+		user.Update()
+		user = QueryByID(user.ID)
+		user.Name = "niky"
+		user.Update()
+		user.Delete()
 	}
 }
 
@@ -54,13 +73,32 @@ func BenchmarkConcurrencyUser(b *testing.B) {
 
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			user := QueryByUsername("wolffy")
+			user := NewUser("wolffy", "test user", Male, "wolffy", "123456", "78306909@qq.com",
+				"010-12345678", "13520040000", "", set.NewSet(), set.NewSet())
+			user.Save()
+			user = QueryByUsername("wolffy")
 
-			if user != nil {
-				//b.Log("user != nil")
-			} else {
-				b.Error("user == nil")
-			}
+			groups := set.NewSet()
+			groups.Add("111111")
+			groups.Add("22222")
+			user.Groups = groups
+
+			user.Telephone = "010-12345678"
+			user.Update()
+
+			user = QueryByID(user.ID)
+			user.Name = "niky"
+			user.Update()
+			user = QueryByID(user.ID)
+			user.Name = "niky"
+			user.Update()
+			user = QueryByID(user.ID)
+			user.Name = "niky"
+			user.Update()
+			user = QueryByID(user.ID)
+			user.Name = "niky"
+			user.Update()
+			user.Delete()
 		}
 	})
 }
